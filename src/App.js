d6c616371270475d504ac25c819a14f96d8029bc
Joyel Joshy <joyeljoshy33@gmail.com>
2023-04-26 11:46:58 +0530

    stop-video

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import React from 'react';
-import NavBar from './Components/NavBar/NavBar';
-import Banner from './Components/Banner/Banner';
-import Rowpost from './Components/RowPost/Rowpost';
-import { comedy, horror, trending} from './urls'
+import "./App.css";
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import Banner from "./Components/Banner/Banner";
+import Rowpost from "./Components/RowPost/Rowpost";
+import { comedy, horror, trending } from "./urls";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Banner/>
-      <Rowpost url={trending} title='Trendings'/>
-      <Rowpost url={horror} title='Horror' isSmall/>
-      <Rowpost url={comedy} title='Comedy' isSmall/>
+      <NavBar />
+      <Banner />
+      <Rowpost url={trending} title="Trendings" />
+      <Rowpost url={horror} title="Horror" isSmall />
+      <Rowpost url={comedy} title="Comedy" isSmall />
     </div>
   );
 }
--- src/Components/RowPost/Rowpost.jsx
+++ src/Components/RowPost/Rowpost.jsx
@@ -1,83 +1,97 @@
-import React, { useEffect, useState } from 'react'
-import './Rowpost.css'
-import axios from '../../axios'
-import {imageUrl,API_KEY} from '../../Constants/constant'
-import Youtube from 'react-youtube'
+import React, { useEffect, useState } from "react";
+import "./Rowpost.css";
+import axios from "../../axios";
+import { imageUrl, API_KEY } from "../../Constants/constant";
+import Youtube from "react-youtube";
 
 function Rowpost(props) {
-  const [movies, setMovies] = useState([])
-  const [urlId, setUrlId] = useState('')
-  const [idx, setIdx] = useState('')
+  const [movies, setMovies] = useState([]);
+  const [urlId, setUrlId] = useState("");
+  const [idx, setIdx] = useState("");
   useEffect(() => {
-    axios.get(props.url).then((response)=>{
-      console.log(response.data.results[0]);
-      setMovies(response.data.results)
-    }).catch((err)=>{
-      // alert('error')
-    })
-  }, [])
-  
+    axios
+      .get(props.url)
+      .then((response) => {
+        console.log(response.data.results[0]);
+        setMovies(response.data.results);
+      })
+      .catch((err) => {
+        // alert('error')
+      });
+  }, []);
+
   function onReady(event) {
-    event.target.setPlaybackQuality('hd1080');
+    event.target.setPlaybackQuality("hd1080");
     // event.target.playVideo();
   }
 
   const opts = {
-    height: '152',
-    width: '250',
+    height: "152",
+    width: "250",
     playerVars: {
-      controls:0,
+      controls: 0,
       autoplay: 1,
-      showinfo:0,
+      showinfo: 0,
       modestbranding: 1,
-    }
-  }
+    },
+  };
   const opts1 = {
-    height: '252',
-    width: '450',
+    height: "252",
+    width: "450",
     playerVars: {
-      controls:0,
+      controls: 0,
       autoplay: 1,
-      showinfo:0,
+      showinfo: 0,
       modestbranding: 1,
-    }
-  }
-  const handleMovie = (id,i)=>{
+    },
+  };
+  const handleMovie = (id, i) => {
     setIdx(i);
     console.log(id);
-    axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response)=>{
-      console.log(response.data);
-      if (response.data.results.length!==0) {
-        setUrlId(response.data.results[0].key)
-        console.log(urlId);
-      } else {
-        setUrlId('')
-        console.log('no video');
-      }
-    })
-  }
+    axios
+      .get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.results.length !== 0) {
+          setUrlId(response.data.results[0].key);
+          console.log(urlId);
+        } else {
+          setUrlId("");
+          console.log("no video");
+        }
+      });
+  };
 
   return (
-    <div className='row'>
-        <h2>{props.title}</h2>
-        <div className='posters'>
-          {movies.map((movie,i)=>{
-            return(
-              <div>
-                {(idx===i && urlId) ? <Youtube className={props.isSmall?'small-poster':'poster'} videoId={urlId} 
-                opts={props.isSmall ? opts : opts1} onReady={onReady}/>:  
-              <img onClick={()=>{
-                handleMovie(movie.id,i)
-              }} className={props.isSmall?'small-poster':'poster'} src={`${imageUrl+movie.backdrop_path}`} alt="poster" />
-            }
-              </div>
-            )
-          })}
-        </div>
-        
-        
+    <div className="row">
+      <h2>{props.title}</h2>
+      <div className="posters">
+        {movies.map((movie, i) => {
+          return (
+            <div onMouseLeave={()=>setUrlId(null)}>
+              {idx === i && urlId ? (
+                <Youtube
+                  className={props.isSmall ? "small-poster" : "poster"}
+                  videoId={urlId}
+                  opts={props.isSmall ? opts : opts1}
+                  onReady={onReady}
+                  />
+              ) : (
+                <img
+                  onClick={() => {
+                    handleMovie(movie.id, i);
+                  }}
+                  className={props.isSmall ? "small-poster" : "poster"}
+                  src={`${imageUrl + movie.backdrop_path}`}
+                  alt="poster"
+                />
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Rowpost
+export default Rowpost;
